3da550ad741de1f866d439ba95cd95944be04bd3
Don Schemmel <dons500@gmail.com>
2015-09-10 18:40:49 -0700

    Pass tests for numberes 1-10

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var romanNum = function(num) {
   var output = '';
-  var romanNumber = ["I"];
-  var numbers = [1];
-  for (var i = 0; i < numbers.length; i++) {
+  // Set up key numerals and numeral pairs
+  var romanNumber = ["X","IX","V","IV","I"];
+  var numbers = [10,9,5,4,1,];
+  // Loop through each of the values to diminish the number
+  for (var i = 0; i < 13; i++) {
+    // If the number being converted is less than the test value, append
+    // the corresponding numeral or numeral pair to the resultant string
     while(num >= numbers[i]) {
       output += romanNumber[i];
       num -= numbers[i];
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,20 @@ describe('romanNum', function() {
   it("Takes 2 and converts it to II", function() {
     expect(romanNum(2)).to.equal("II")
   });
+
+  it("Takes 4 and subtacts 1 from 5 to make IV", function() {
+    expect(romanNum(4)).to.equal("IV")
+  });
+
+  it("Takes 5 and and converts it to V", function() {
+    expect(romanNum(5)).to.equal("V")
+  });
+
+  it("Takes 9 and subtacts 1 from 10 to make IX", function() {
+    expect(romanNum(9)).to.equal("IX")
+  });
+
+  it("Takes 10 and converts it to X", function() {
+    expect(romanNum(10)).to.equal("X")
+  });
 });
